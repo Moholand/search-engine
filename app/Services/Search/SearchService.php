d95fdb5524205aea303554c85ae1be888f89b22f
Mohammad Akbarzadeh <moho.akbarzadeh@gmail.com>
2023-01-06 21:36:32 +0330

    Clean Paginator

--- app/Services/Search/SearchService.php
+++ app/Services/Search/SearchService.php
@@ -2,9 +2,9 @@
 
 namespace App\Services\Search;
 
-use App\Models\Product;
 use Elastic\Elasticsearch\Client;
 use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Arr;
 
 class SearchService
@@ -33,25 +33,16 @@ class SearchService
         $params = $this->getParams($queryArray, $perPage, $page);
 
         $result = $this->client->search($params);
-        $total = $result['hits']['total'];
 
-        if (isset($result['hits']['hits'])) {
-            $result_data = Arr::pluck($result['hits']['hits'], '_source');
-        }
-
-        $url = 'api/products/search?title=';
-        $urlWithQu = isset($data['title']) ? $url . $data['title'] : $url;
-        $urlWithCat = isset($data['category']) ? $urlWithQu . '&category=' . $data['category'] : $urlWithQu;
-        $urlWithPrice = isset($data['price_from']) || isset($data['price_to'])
-            ? $urlWithCat . '&price_from=' . $data['price_from'] . '&price_to=' . $data['price_to'] : $urlWithCat;
-
-        return new LengthAwarePaginator (
-            $result_data,
-            $total['value'],
+        $products = new LengthAwarePaginator (
+            $result['hits']['hits'],
+            $result['hits']['total']['value'],
             $perPage,
-            $page,
-            ['path' => url($urlWithPrice)]
+            Paginator::resolveCurrentPage(),
+            ['path' => Paginator::resolveCurrentPath()]
         );
+
+        return $this->loadProducts($products->withQueryString());
     }
 
     public function recommendations(string $title): ?array
@@ -136,5 +127,23 @@ class SearchService
             ]
         ];
         return $queryArray;
+    }
+
+    public function loadProducts($products): LengthAwarePaginator
+    {
+        foreach ($products as $key => $product) {
+            $product_item['_score'] = $product['_score'];
+            $product = $product['_source'];
+
+            $product_item['id'] = $product['id'];
+            $product_item['title'] = $product['title'];
+            $product_item['price'] = $product['price'];
+            $product_item['point'] = $product['point'];
+            $product_item['image'] = $product['image'];
+
+            $products[$key] = $product_item;
+        }
+
+        return $products;
     }
 }

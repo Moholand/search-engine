3c0b2e4db13d1689ff815d96673fa5c9c42e974a
Mohammad Akbarzadeh <moho.akbarzadeh@gmail.com>
2025-04-18 23:30:51 +0330

    fix: reactivity issue

--- resources/js/components/products/Show/ProductDetail.vue
+++ resources/js/components/products/Show/ProductDetail.vue
@@ -25,8 +25,26 @@
                             <span class="px-2">تومان</span>
                         </div>
                     </div>
-                    <div class="order-btn my-5" @click="addToCart">
+                    <div class="order-btn my-5" @click="addToCart" v-if="countInCart == 0">
                         افزودن به سبد
+                    </div>
+                    <div class="counter my-5" v-else>
+                        <span class="signs increase d-flex align-items-center" @click="changeItemCount(increase)">
+                            <i class="fa-solid fa-plus fa-xs"></i>
+                        </span>
+                        <div>
+                            <loading :is-loading="isLoading" v-if="isLoading" />
+                            <span class="number" v-else>
+                                {{ countInCart }}
+                            </span>
+                        </div>
+                        <span
+                            class="signs decrease d-flex align-items-center"
+                            @click="countInCart === 1 ? deleteItem() : changeItemCount(decrease)"
+                        >
+                            <i class="fa-solid fa-trash fa-xs" v-if="countInCart === 1"></i>
+                            <i class="fa-solid fa-minus fa-xs" v-else></i>
+                        </span>
                     </div>
                 </div>
             </div>
@@ -42,11 +60,15 @@
 
 <script>
 import alertMixin from '@/helpers/mixins/alertMixin';
+import { HTTP_STATUS_CODES } from '@/helpers/constants/httpStatusCodes';
 
 export default {
     data() {
         return {
             product: null,
+            countInCart: 0,
+            increase: 'increase',
+            decrease: 'decrease',
             isLoading: false,
             benefits: [
                 { logo: 'express-delivery.svg', text: 'امکان تحویل اکسپرس' },
@@ -67,7 +89,8 @@ export default {
             this.isLoading = true;
 
             try {
-                this.product = (await axios.get('/api/products/' + this.$route.params.id)).data
+                this.product = (await axios.get('/api/products/' + this.$route.params.id)).data;
+                this.countInCart = this.product.count_in_user_cart;
             } catch (error) {
                 this.showAlert(this.errorMessage, 'error');
             } finally {
@@ -77,11 +100,38 @@ export default {
         async addToCart() {
             try {
                 let response = (await axios.post('/api/carts/products/' + this.product.id)).data;
+                this.countInCart = 1;
                 this.emitter.emit('cart-count-update', { type: 'increase' });
                 this.showAlert(response.response_message, 'success');
             } catch (error) {
                 this.showAlert(this.errorMessage, 'error');
             }
+        },
+        changeItemCount(type) {
+            this.isLoading = true;
+            axios.patch(`/api/carts/products/${this.product.id}/changeCount`, { type })
+                .then(response => {
+                    if (response.status == HTTP_STATUS_CODES.OK) {
+                        this.countInCart += (type === this.increase ? 1 : -1);
+                    }
+                })
+                .catch(() => this.showError())
+                .finally(() => this.isLoading = false);
+        },
+        deleteItem() {
+            this.isLoading = true;
+            axios.delete(`/api/carts/products/${this.product.id}`)
+                .then(response => {
+                    if (response.status == HTTP_STATUS_CODES.OK) {
+                        // this.$emit('deleteItem', this.cartItem.pivot.product_id);
+                        // this.$emit('showAlert', response.data.response_message, 'success');
+                    }
+                })
+                .catch(() => this.showError())
+                .finally(() => this.isLoading = false);
+        },
+        showError() {
+            this.$emit('showAlert', this.errorMessage, 'error');
         }
     }
 }
@@ -151,6 +201,25 @@ export default {
         font-size: 11px;
         font-weight: bold;
         color: #9e9fb1;
+    }
+    .counter {
+        border: 1px solid #e0e0e2;
+        border-radius: 10px;
+        padding: 5px 10px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        color: #ef4056;
+        position: relative;
+        min-height: 41px;
+        max-width: 300px;
+    }
+    .counter .number {
+        font-size: 16px;
+    }
+    .signs {
+        font-size: 20px;
+        cursor: pointer;
     }
     /* Styles for mobile devices */
     @media only screen and (max-width: 767px) {
